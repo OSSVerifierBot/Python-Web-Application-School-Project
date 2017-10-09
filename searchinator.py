53830fd6d45005daf564ed59b75239f934c29e10
Charles Hasty <cth3md@mst.edu>
2017-10-08 19:31:01 -0500

    login function works

--- searchinator.py
+++ searchinator.py
@@ -19,23 +19,38 @@ get scared when they see tracebacks.
 
 """
 import uuid
+import functools
+import operator
 
 from flask import abort, Flask, flash, redirect, request, session, url_for
 from datetime import datetime
 
-from utils import add_data_param, login_required, uses_template
+from utils import add_data_param, uses_template, login_required
+
+#, login_required, uses_template
 from condition import Condition
 
 app = Flask(__name__)
 app.secret_key = 'very.secret'
 app.config['DATA_PATH'] = 'inator_data.json'
 
-
+@app.route('/')
+@login_required
+@add_data_param(app.config['DATA_PATH'])
+@uses_template('list-inators.html')
 def list_inators(data):
     """List all inators."""
-    return {}
+    # Sorting inators by name
+    lst = sorted(data['inators'].values(), key = lambda x: x['name'])
+    # Sorting inators by condition
+    lst = sorted(lst, key = lambda x: x['condition'], reverse=True)
+    inators = {'inators':lst}
+
+    return inators
 
 
+@app.route('/add/')
+@add_data_param(app.config['DATA_PATH'])
 def add_inator(data):
     """Add a new inator."""
     if request.method == 'GET':
@@ -44,11 +59,15 @@ def add_inator(data):
         return redirect(url_for('list_inators'))
 
 
+@app.route('/view/<ident>/')
+@add_data_param(app.config['DATA_PATH'])
 def view_inator(data, ident):
     """View details of an inator."""
     return {}
 
 
+@app.route('/delete/<ident>/')
+@add_data_param(app.config['DATA_PATH'])
 def delete_inator(data, ident):
     """Delete an existing inator."""
     if request.method == 'GET':
@@ -56,15 +75,42 @@ def delete_inator(data, ident):
     if request.method == 'POST':
         return redirect(url_for('list_inators'))
 
-
+@app.route('/login/', methods=['GET', 'POST'])
+@add_data_param(app.config['DATA_PATH'])
+@uses_template('login.html')
 def login(data):
     """Login to the searchinator."""
     if request.method == 'GET':
         return {}
     if request.method == 'POST':
-        return redirect(url_for('list_inators'))
+        if 'username' in session:
+            flash('You are already logged in. Log out to log in again.', 'danger')
+            return redirect(url_for('list_inators'))
+        username = request.form['username']
+        try:
+            user = data['users'][username]
+        except KeyError:
+            flash('Cannot find user {}. Try again.'.format(username), 'danger')
+            return redirect(url_for('login'))
 
+        try:
+            correct_password = user['password']
+        except KeyError:
+            flash('Cannot find password for user{}!'.format(username), 'danger')
+            return redirect(url_for('login'))
 
+        password = request.form['password']
+        if password == correct_password:
+            session['username'] = username
+            flash('Successfully logged in as {}.'.format(username), 'success')
+            return redirect(url_for('list_inators'))
+        else:
+            flash('Incorrect password for user{}.'.format(username), 'danger')
+            return redirect(url_for('login'))
+
+@app.route('/logout/')
+@login_required
+@uses_template('logout.html')
 def logout():
     """Logout of the searchinator."""
     if request.method == 'GET':
